99e7d42640e262a1ddaa4a32672cc47da22bd7b1
Sadaf <sadafara@gmail.com>
2024-12-20 16:15:44 +0530

    Bug Fix: Nav Bar responsive design

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ explorebtn.addEventListener("click",()=>{
     let dropdown = document.querySelector(".dropdown-content");
     dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex';
 })
+let toggle = document.getElementById("toggle");
+toggle.addEventListener("click", () => {
+    let appear = document.querySelector(".dropdown-content");
+    appear.style.display = appear.style.display === 'flex' ? 'none' : 'flex';
+});
+
 
 
